1a0be8a475c09fdd0f2d40205b869dd0b347b188
Karel Vanden Houte <karel.vandenhoute@gmail.com>
2018-02-26 17:32:19 +0000

    import fix

--- plugins/che-wizard-sample/che-wizard-sample-server/src/main/java/org/eclipse/che/sample/handler/JujuCreateProjectHandler.java
+++ plugins/che-wizard-sample/che-wizard-sample-server/src/main/java/org/eclipse/che/sample/handler/JujuCreateProjectHandler.java
@@ -5,6 +5,7 @@ import static org.eclipse.che.sample.shared.Constants.JUJU_PROJECT_TYPE_ID;
 
 import com.google.inject.Inject;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import org.eclipse.che.api.core.ConflictException;
 import org.eclipse.che.api.core.NotFoundException;
@@ -21,14 +22,17 @@ import org.eclipse.che.api.project.server.type.AttributeValue;
 public class JujuCreateProjectHandler implements CreateProjectHandler {
 
   @Inject private FsManager fsManager;
+  private static final String FILE_NAME = "package.json";
 
   @Override
   public void onCreateProject(
       String projectPath, Map<String, AttributeValue> attributes, Map<String, String> options)
       throws ConflictException, ServerException {
 
-    try (InputStream personJson = getClass().getClassLoader().getResourceAsStream("files/test")) {
+    try (InputStream packageJson = getClass().getClassLoader().getResourceAsStream("files/test");
+        InputStream personJson = getClass().getClassLoader().getResourceAsStream("files/test")) {
       String projectWsPath = WsPathUtils.absolutize(projectPath);
+      fsManager.createFile(resolve(projectWsPath, FILE_NAME), packageJson);
 
       String myJsonFilesWsPath = resolve(projectWsPath, "myJsonFiles");
       fsManager.createFile(resolve(myJsonFilesWsPath, "person.json"), personJson);
